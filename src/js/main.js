d35f927fd7ccdbbc6cd5c3412f29ec168abb373e
jovana.marceta <jmarcetafedev@gmail.com>
2023-03-05 19:08:28 +0100

    Adding tabs section and implement logic

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,10 @@ import './app.js';
 
 import './app.js';
 import './modal.js';
+import './tabs.js';
 
 require('./nav');
 require('./modal');
+require('./tabs');
 
 
---
+++ src/js/tabs.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.tabTarget)
+
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove('active-tab')
+		})
+
+		tabs.forEach(tab => {
+			tab.classList.remove('active-tab')
+		})
+
+		tab.classList.add('active-tab')
+		target.classList.add('active-tab')
+	})
+})
